423e9d202612cc8f9c08fdc785376a7f144f2083
BrokenAlias <mathias.berntsen@gmail.com>
2025-11-02 21:50:35 +0100

    pull commit

--- control.gd
+++ control.gd
@@ -3,13 +3,16 @@ extends Control
 
 @onready var button: TextureButton = $TextureButton
 @onready var scorelabel: Label = $Label
+
 var cash = 0
 var cashPerSec = 1
+
 
 func save_game():
 	var file = FileAccess.open("user://save_game.dat", FileAccess.WRITE)
 	file.store_line(str(cash))
 	file.close()
+
 
 func load_game():
 	if FileAccess.file_exists("user:///save_game.dat"):
@@ -19,10 +22,11 @@ func load_game():
 		scorelabel.text = saved_score
 		file.close()
  
+
 func _ready() -> void:
 	load_game()
 
-	
+
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_WM_CLOSE_REQUEST:
 		save_game()
